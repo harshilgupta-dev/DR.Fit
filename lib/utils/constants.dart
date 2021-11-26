2c73eed0d4d084d097c263a9a4867522870c941f
Harsh Tripathi <ht87374@gmail.com>
2021-11-27 03:17:10 +0530

    added more constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -12,3 +12,4 @@ const kTextColor = Color(0xFF222B45);
 const kBlueLightColor = Color(0xFFC7B8F5);
 const kBlueColor = Color(0xFF817DC0);
 const kShadowColor = Color(0xFFE6E6E6);
+const homePageColor = Color(0xFFF5CEB8);
