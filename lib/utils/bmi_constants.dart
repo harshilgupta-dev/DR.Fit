dd95bf26246e69add50eda82d0ff514ac8374b76
Harshil Gupta <harshilgupta0099@gmail.com>
2021-11-29 22:58:01 +0530

    bmi constants

---
+++ lib/utils/bmi_constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Colors.white70;
+const kInactiveCardColour = Colors.black26;
+const kBottomContainerColour = Colors.white;
+
+const kNumberTextStyle = TextStyle(fontWeight: FontWeight.w900, fontSize: 50.0);
+
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D876), fontSize: 20.0, fontWeight: FontWeight.bold);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
