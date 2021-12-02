b834bd8cf98c874f56c776152553b610635d5793
Harshil Gupta <harshilgupta0099@gmail.com>
2021-12-03 01:21:35 +0530

    add new elevated button

---
+++ lib/widgets/new_elevated_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NewElevatedButton extends StatelessWidget {
+  const NewElevatedButton(
+      {Key? key,
+      required this.color,
+      required this.icon,
+      required this.label,
+      required this.press})
+      : super(key: key);
+
+  final Color color;
+  final VoidCallback press;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(elevation: 10, primary: color),
+        onPressed: press,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+          width: size.width * .8,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(icon),
+              SizedBox(
+                width: size.height * .01,
+              ),
+              Text(label,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ))
+            ],
+          ),
+        ));
+  }
+}
