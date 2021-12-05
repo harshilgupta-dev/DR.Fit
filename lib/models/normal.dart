ee46ae9e89d05b3e3e42c75628f390aeb009b105
Harshil Gupta <harshilgupta0099@gmail.com>
2021-12-05 22:17:02 +0530

    update

--- lib/models/normal.dart
+++ lib/models/normal.dart
@@ -25,17 +25,17 @@ const List<NormalWeight> normalWeightList = [
       'Day Two',
       'assets/images/underweight/daytwo.jpg',
       'Breakfast\nChia Porridge with Fruit: 2 tbsp chia seeds + rolled oats + 1.5 cup full cream milk + 1 serve of fruit (eg. 1 medium banana or 6 dried prunes).\n\nLunch\nEgg, Cheese and Salad Wrap: 2 boiled & mashed eggs + 2 cheese slices + avocado + 1 cup salad vegetables (eg. lettuce, cucumber, carrot, capsicum) rolled up in tortilla bread (make 2 wraps) + 1 cup fruit juice.\n\nDinner\nBaked Salmon, Cous Cous and Vegetables: Oven baked salmon fillet, sprinkled with sesame oil and sesame seeds + wholemeal cous cous + 2 cups cooked vegetables. Serve with hommus.\n\nDessert/Supper\nFruit Salad Dessert: Fresh fruit salad with ice cream.\n\nSnacks\nA handful of mixed nuts and/or seeds. Fruit toast with butter and/or jam.',
-      'Normalweight'),
+      'Normal Weight'),
   NormalWeight(
       3,
       'Day Three',
       'assets/images/underweight/daythree.jpg',
       'Breakfast\nSweet Potato Spanish Omelette (1 serve): Serve with 1 cafe latte or hot chocolate made with full cream milk.\n\nLunch\nLentil, Vegetables and Barley Soup: Lentils with 1 cup chopped vegetables (eg. carrot, pumpkin, celery, onion), vegetable stock and barley. Serve with dollop of natural yoghurt. + 1 serve of fruit.\n\nDinner\nSpaghetti Bolognese: Lean mince with tomato, tomato puree, herbs & garlic served on spaghetti and sprinkled with cheese + 1 cup salad (eg. baby spinach, carrots, cucumber) with olive oil based dressing.\n\nDessert/Supper\nFruit Smoothie: Made with 1 cup milk + 1 tbsp walnuts + fruit (eg. a small mango or berries).\n\nSnacks\n20g cheese + Canned tuna in oil on wholegrain crackers.',
-      'Normalweight'),
+      'Normal Weight'),
   NormalWeight(
       4,
       'Day Four',
       'assets/images/underweight/dayfour.png',
       'Breakfast\nWholegrain Cereal with Milk and Fruit: Wholegrain flaky cereal + full cream milk + 2 tbsp linseeds + 1 serve of fruit (eg. 6 dried apricot halves or 4 small plums).\n\nLunch\nChicken and Noodle Stir-fry: Sliced lean chicken + Hokkein noodles + 1 cup vegetables (eg. beans, capsicum, spinach, carrot) cooked in sesame oil + sweet soy sauce dressing. Sprinkle with sesame seeds.\n\nDinner\nNasi Goreng Tray Bake (1 serve): Serve with 2 cup mixed salad vegetables.\n\nDessert/Supper\nFruit Pop: Made with frozen fruits (eg. bananas or mangos) with Greek yoghurt.\n\nSnacks\n1 serve of fruit (eg. 5 prunes or 1 cup fresh fruit salad). 1 cup hot chocolate made with full cream milk.',
-      'Normalweight'),
+      'Normal Weight'),
 ];
