9d9a8a4e596aa75f3026fb4b664669a1fd938ad4
Harsh Tripathi <ht87374@gmail.com>
2021-12-05 13:16:05 +0530

    added models

---
+++ lib/models/overweight.dart
@@ -0,0 +1,47 @@
+class OverWeight {
+  final int id;
+  final String name;
+  final String image;
+  final String step;
+  final String category;
+  const OverWeight(
+    this.id,
+    this.name,
+    this.image,
+    this.step,
+    this.category,
+  );
+}
+
+const List<OverWeight> overWeightList = [
+  OverWeight(
+      1,
+      'Day One',
+      'assets/images/overweight/dayone.jpg',
+      'Breakfast:\nOne bowl of watermelon/kiwi or an apple/pomegranate\n\nLunch:\nOne bowl of papaya or muskmelon\n\nEvening Snack:\nOne glass of coconut water\n\nDinner:\nOne guava/ orange or a bowl of berries (strawberries, litchi)\n\nBed-time snacks:\nOne bowl of watermelon/grapes\n\n\nDon’ts:\nAvoid any kind of cream, honey or sugar dressing on your fruit bowl.',
+      'Overweight'),
+  OverWeight(
+      2,
+      'Day Two',
+      'assets/images/overweight/daytwo.jpg',
+      'Breakfast: One large or two small-sized potatoes/ bowl of corn kernels or green peas\n\nLunch: One large bowl of cabbage soup\n\nEvening Snack: 2-3 medium-sized cherry tomatoes\n\nDinner: One cup broccoli \n\nBed-time snacks: One bowl of cucumber/ carrots\n\n\nDon’ts: Restrict consumption of potatoes or any other starch containing vegetables like corn or peas only to the breakfast and in limited amounts (100-150 calories) as it will provide enough energy throughout the day. Avoid eating deep-fried vegetables.‍',
+      'Overweight'),
+  OverWeight(
+      3,
+      'Day Three',
+      'assets/images/underweight/daythree.jpg',
+      'Breakfast: One bowl of watermelon/ or an apple or about half a pineapple\n\nLunch: One large bowl of cabbage soup\n\nEvening Snack: 2-3 medium sized cherry tomatoes\n\nDinner: One cup broccoli \n\nBed-time snacks: One bowl of cucumber/ carrots\n\n\nDon’ts: Avoid bananas, mangoes and potatoes, corn or peas.',
+      'Overweight'),
+  OverWeight(
+      4,
+      'Day Four',
+      'assets/images/overweight/dayfour.jpg',
+      'Breakfast: Two large bananas with a glass of skimmed milk (750 ml)\n\nLunch: Two large bananas with a glass of skimmed milk\n\nEvening Snack: A bowl of cabbage soup.\n\nDinner: Two large bananas with a glass of skimmed milk\n\nBed-time snacks: Nothing\n\n\nDon’ts: Avoid adding any sweeteners, honey or sugar to the milk. Do not put cornstarch in the soup. Mostly prefer skimmed milk.',
+      'Overweight'),
+  OverWeight(
+      5,
+      'Day Five',
+      'assets/images/overweight/dayfive.jpg',
+      'Breakfast: Three whole tomatoes\n\nLunch: 250 gm of meat of your choice with one large tomato\n\nEvening Snack: Nothing\n\nDinner: A bowl of Tomato soup\n\nBed-time snacks: Nothing\n\n\nSubstitutes: Choice of meat may include beef, chicken, eggs or fish. Vegetarians may substitute meat with cottage cheese, tofu and brown rice.',
+      'Overweight'),
+];
